e67ce88691788f5937ba321b594f1e0ea40c253b
Gaurav <gauravsinghbora445@gmail.com>
2025-12-29 16:01:18 +0530

    condtional

--- Day01/conditional.py
+++ Day01/conditional.py
@@ -1,9 +1,51 @@
+# number = int(input("Enter the number : "))
+
+# Write a program to check whether a number is positive or negative.
+# if number > 0:
+#     print("Number is Poistive")
+# elif number < 0:
+#     print("Number is Negtive")
+# else :
+#     print("Number is Zero")
+    
+
+# Write a program to check whether a number is even or odd.
+# if number % 2== 0:
+#     print("Number is even")
+# else:
+#    print("Number is odd")
+   
+
+# Check whether a given number is zero or non-zero.
+# if number == 0 :
+#     print("number is zero")
+# else :
+#     print("number is non-zero")
+
+    
+# Check whether a year is a leap year.
+# if (number % 4 == 0 and number % 100 != 0) or (number % 400 == 0):
+#     print("Leap year") 
+# else:
+#     print("Not a leap year")
+
+# Check whether a character is a vowel or consonant.
+ch = input("Enter a character: ").lower()
+
+if ch in ('a', 'e', 'i', 'o', 'u'):
+    print("Vowel")
+else:
+    print("Consonant")
+# Check whether a number is single-digit, double-digit, or more.
 number = int(input("Enter the number : "))
 
-if number > 0:
-    print("Number is Poistive")
-elif number < 0:
-    print("Number is Negtive")
-else :
-    print("Number is Zero")
-    
+
+if -9 <= number <= 9:
+    print("Single-digit number")
+elif -99 <= number<= 99:
+    print("Double-digit number")
+else:
+    print("More than double-digit number")
+
+
+# Check whether a given number is multiple of both 3 and 7.
