50ad040028d5b95f75ff6c8ca94ff48067b773ad
Gaurav <gauravsinghbora445@gmail.com>
2025-12-29 15:25:46 +0530

    user input

---
+++ Day01/userInput.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age : "))
+name =input("Enter your name : ")
+
+future_age= age + 10
+
+print("Hello",name, "you are ",age," years old.")
+print("After 10 years age will be",future_age)
+
