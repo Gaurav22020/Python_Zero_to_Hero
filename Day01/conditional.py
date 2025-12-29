671d8ee15fb9f04d8b0409445f8602bece95ee42
Gaurav <gauravsinghbora445@gmail.com>
2025-12-29 15:38:42 +0530

    condtional

---
+++ Day01/conditional.py
@@ -0,0 +1,9 @@
+number = int(input("Enter the number : "))
+
+if number > 0:
+    print("Number is Poistive")
+elif number < 0:
+    print("Number is Negtive")
+else :
+    print("Number is Zero")
+    
