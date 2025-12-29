25cfdcb62ff383bb3d728caf615d39e2dca83859
Gaurav <gauravsinghbora445@gmail.com>
2025-12-29 16:13:50 +0530

    condtional

--- Day01/conditional.py
+++ Day01/conditional.py
@@ -30,22 +30,29 @@
 #     print("Not a leap year")
 
 # Check whether a character is a vowel or consonant.
-ch = input("Enter a character: ").lower()
+# ch = input("Enter a character: ").lower()
 
-if ch in ('a', 'e', 'i', 'o', 'u'):
-    print("Vowel")
-else:
-    print("Consonant")
+# if ch in ('a', 'e', 'i', 'o', 'u'):
+#     print("Vowel")
+# else:
+#     print("Consonant")
 # Check whether a number is single-digit, double-digit, or more.
-number = int(input("Enter the number : "))
+# number = int(input("Enter the number : "))
 
 
-if -9 <= number <= 9:
-    print("Single-digit number")
-elif -99 <= number<= 99:
-    print("Double-digit number")
-else:
-    print("More than double-digit number")
+# if -9 <= number <= 9:
+#     print("Single-digit number")
+# elif -99 <= number<= 99:
+#     print("Double-digit number")
+# else:
+#     print("More than double-digit number")
 
 
 # Check whether a given number is multiple of both 3 and 7.
+number = int(input("Enter the number : "))
+if number % 3 == 0 and number % 7 ==0:
+    print(number, "is divisible of 3 and 7")
+else :
+    print("not divisible both 3 and 7") 
+    
+       
